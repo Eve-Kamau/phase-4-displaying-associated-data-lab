37600f70a9444b5d491c43d64320c31b94439b10
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-05-28 15:22:52 +0300

    Completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+def index
+    users = User.all
+    render json: users
+end
+
+def show
+user = User.find_by(params[:id])
+render json: user,include: :items, except:[:user_id, :created_at, :updated_at]
+end
+
 end
